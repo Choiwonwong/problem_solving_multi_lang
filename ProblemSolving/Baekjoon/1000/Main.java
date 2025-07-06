94959ea5a827d2e3c70b74b2e3a365dd23f8eecd
Choiwonwong <chldnjsdnd12@gmail.com>
2025-07-06 23:36:58 +0900

    add ruff for python

--- ProblemSolving/Baekjoon/1000/Main.java
+++ ProblemSolving/Baekjoon/1000/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Java 버전 출력
         System.out.println("Java Version: " + System.getProperty("java.version"));
+        System.out.println("Java Class Version: " + System.getProperty("java.class.version"));
 
         int a = 1;
         int b = 2;
--- ProblemSolving/Baekjoon/1000/main.py
+++ ProblemSolving/Baekjoon/1000/main.py
@@ -1,6 +1,6 @@
 import sys
 
-a = 1 
+a = 1
 b = 2
 print(a + b)
 print(sys.version)
