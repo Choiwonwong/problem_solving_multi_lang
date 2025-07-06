2a1a61a9efbdfc252596c531738b96e833cc4fe9
Choiwonwong <chldnjsdnd12@gmail.com>
2025-07-06 22:52:36 +0900

    set-up JDK and check version

--- ProblemSolving/Baekjoon/1000/Main.java
+++ ProblemSolving/Baekjoon/1000/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        // Java 버전 출력
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+
         int a = 1;
         int b = 2;
         System.out.println(a + b);
--- ProblemSolving/Baekjoon/1000/main.py
+++ ProblemSolving/Baekjoon/1000/main.py
@@ -1,3 +1,6 @@
+import sys
+
 a = 1 
 b = 2
 print(a + b)
+print(sys.version)
